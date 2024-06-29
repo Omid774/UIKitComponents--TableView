248416458e9aebe3e6f2e412ab2543a092d98b58
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-06-29 11:37:27 +0330

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Omid Heydarzadeh on 6/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
